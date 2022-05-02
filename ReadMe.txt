e4b542ce880df390ed9e32d04e6dcd9120807e90
Tanaka Tsoka <tanaka@basalt.co>
2022-05-02 23:57:56 +0200

    Initial Commit

---
+++ ReadMe.txt
@@ -0,0 +1,13 @@
+1. Open text file coordinates.txt
+2. Enter Grid Zone, Starting locations and Orientation/Direction, Movement Commands.
+NB.Each line must not contain any spaces.
+
+Coordinates file input example 
+Format example:
+88 //Grid Zone
+12E //Starting Location 1 horizontal axis and 2 vertical axis, and E for East. 
+MMLMRMMRRMML/// Movement in which the rover will go (M) is Move, L ( turns 90degrees Left) and R( tuns 90degrees Right)
+
+3. to run open Terminal and run node app
+
+and SWEEET CAROLINEEEEEEE DAH DAH DAH.
---
+++ app.js
@@ -0,0 +1,54 @@
+var fs =  require('fs')
+var readMe = fs.readFileSync('coordinates.txt', 'utf8');
+var grid = readMe.split('\n')[0];
+var bounds = [];
+for(var line = 0; line < grid.length; line++){
+    bounds.push(grid[line])
+}
+let input = readMe.split('\n')[2];
+
+let startingLocation = readMe.split('\n')[1];
+for (var i = 0; i < startingLocation.length; i++) {
+    console.log(startingLocation.charAt(i));
+  }
+const directions = ["N", "E", "S", "W"];
+let x = Number(startingLocation.charAt(0)), y = Number(startingLocation.charAt(1));
+let direction = directions.findIndex((d) => d == startingLocation.charAt(2));
+
+const directionMovement = [
+  [0, 1],
+  [1, 0],
+  [0, -1],
+  [-1, 0],
+];
+
+let isValid = true;
+
+input.split("").forEach((instruction) => {
+  if (!isValid) return;
+
+  switch (instruction) {
+    case "L":
+      if (direction > 0) direction--;
+      else direction = 3;
+      break;
+    case "R":
+      if (direction < 3) direction++;
+      else direction = 0;
+      break;
+    case "M":
+      var movement = directionMovement[direction];
+      x += movement[0];
+      y += movement[1];
+      break;
+    default:
+      break;
+  }
+  if (x < 0 || x >= bounds[0]) isValid = false;
+  if (y < 0 || y >= bounds[1]) isValid = false;
+});
+
+if (isValid) 
+  console.log([x, y], directions[direction]);
+else 
+  console.log("Out of bounds");
